87297f6ae9afd5c7c79bae748e6d87e7f5b40b2f
tu15007959375 <1804694379@qq.com>
2023-11-05 16:41:34 +0800

    查看购物车

--- springboot/heima/take-out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ springboot/heima/take-out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -66,4 +66,18 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         }
 
     }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long currentId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(currentId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
