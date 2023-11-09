bb930b536611d2ba8372abd07b29407f6e01e8d6
tu15007959375 <1804694379@qq.com>
2023-11-09 20:47:39 +0800

    派送订单与完成订单

--- springboot/heima/take-out/sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
+++ springboot/heima/take-out/sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -77,4 +77,16 @@ public interface OrderService {
      * 取消订单
      */
     void cancel(OrdersCancelDTO ordersCancelDTO);
+
+    /**
+     * 派送订单
+     * @param id
+     */
+    void delivery(Long id);
+
+    /**
+     * 完成订单
+     * @param id
+     */
+    void complete(Long id);
 }
--- springboot/heima/take-out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ springboot/heima/take-out/sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -276,6 +276,53 @@ public class OrderServiceImpl implements OrderService {
 
 
     /**
+     * 派送订单
+     *
+     * @param id
+     */
+    @Override
+    public void delivery(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getByOrderId(id);
+
+        // 校验订单是否存在，并且状态为3
+        if (ordersDB == null || !ordersDB.getStatus().equals(Orders.CONFIRMED)) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Orders orders = new Orders();
+        orders.setId(ordersDB.getId());
+        // 更新订单状态,状态转为派送中
+        orders.setStatus(Orders.DELIVERY_IN_PROGRESS);
+
+        orderMapper.update(orders);
+    }
+
+    /**
+     * 完成订单
+     * @param id
+     */
+    @Override
+    public void complete(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getByOrderId(id);
+
+        // 校验订单是否存在，并且状态为4
+        if (ordersDB == null || !ordersDB.getStatus().equals(Orders.DELIVERY_IN_PROGRESS)) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Orders orders = new Orders();
+        orders.setId(ordersDB.getId());
+        // 更新订单状态,状态转为完成
+        orders.setStatus(Orders.COMPLETED);
+        orders.setDeliveryTime(LocalDateTime.now());
+
+        orderMapper.update(orders);
+    }
+
+
+    /**
      * 返回菜品信息，在查看时需要显示菜品相关信息
      * @param page
      * @return
